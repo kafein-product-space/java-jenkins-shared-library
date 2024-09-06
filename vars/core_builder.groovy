205c9aec81d6904b979b4869f42c21f00e518e68
Ferhat Kacar <ferhatkacar.partner@kafein.com.tr>
2024-09-06 20:03:30 +0300

    Updated lib

--- vars/core_builder.groovy
+++ vars/core_builder.groovy
@@ -1,14 +1,13 @@
 def call(Map config) {
-
     def triggerRef = config.containsKey('trigger_ref') ? config.trigger_ref : '$.push.changes[0].old.name'
     def triggerRegexpFilter = config.containsKey('trigger_regexp_filter') ? config.trigger_regexp_filter : '^(development|uat)'
 
-    if ( config.containsKey("github_hook") && config.github_hook ) {
+    if (config.containsKey("github_hook") && config.github_hook) {
         properties([pipelineTriggers([githubPush()])])
     }
 
     pipeline {
-        agent {label config.agent}
+        agent { label config.agent }
 
         options {
             timeout(time: 25, unit: 'MINUTES')
@@ -23,17 +22,16 @@ def call(Map config) {
         triggers {
             GenericTrigger(
                 genericVariables: [
-                    [key: 'REF', value: triggerRef],
+                    [key: 'REF', value: triggerRef]
                 ],
-                 causeString: 'Triggered by Remote Event',
-                 token: 'bitbucket_' + config.sonar_qube_project_key,
-                 printContributedVariables: false,
-                 printPostContent: false,
-                 silentResponse: false,
-                 shouldNotFlattern: false,
-
-                 regexpFilterText: '$REF',
-                 regexpFilterExpression: triggerRegexpFilter
+                causeString: 'Triggered by Remote Event',
+                token: 'bitbucket_' + config.sonar_qube_project_key,
+                printContributedVariables: false,
+                printPostContent: false,
+                silentResponse: false,
+                shouldNotFlatten: false,
+                regexpFilterText: '$REF',
+                regexpFilterExpression: triggerRegexpFilter
             )
         }
 
@@ -41,9 +39,7 @@ def call(Map config) {
             stage("Configure Init") {
                 steps {
                     script {
-                        lib_helper.configureInit(
-                            config
-                        )
+                        lib_helper.configureInit(config)
                     }
                 }
             }
@@ -54,9 +50,7 @@ def call(Map config) {
                         $class: "GitSCM",
                         branches: [[name: config.target_branch.startsWith("refs") ? config.target_branch : "refs/heads/${config.target_branch}"]],
                         submoduleCfg: [],
-                        userRemoteConfigs: [
-                            config.scm_global_config
-                        ]
+                        userRemoteConfigs: [config.scm_global_config]
                     ]
                 }
             }
@@ -64,33 +58,18 @@ def call(Map config) {
             stage("Read Project Config") {
                 steps {
                     script {
-                        // Create config file variable
                         config.config_file = config.containsKey('config_file_path') ? config.config_file_path : ".jenkins/buildspec.yaml"
                         config.b_config = readYaml file: config.config_file
-                        config.job_base = sh(
-                            script: "python3 -c 'print(\"/\".join(\"${JOB_NAME}\".split(\"/\")[:-1]))'",
-                            returnStdout: true
-                        ).trim()
-                        config.job_name = sh(
-                            script: "python3 -c 'print(\"${JOB_NAME}\".split(\"/\")[-1])'",
-                            returnStdout: true
-                        ).trim()
+                        config.job_base = sh(script: "python3 -c 'print(\"/\".join(\"${JOB_NAME}\".split(\"/\")[:-1]))'", returnStdout: true).trim()
+                        config.job_name = sh(script: "python3 -c 'print(\"${JOB_NAME}\".split(\"/\")[-1])'", returnStdout: true).trim()
 
-                        // Configure commit ID for project
-                        commitID = sh(
-                            script: """
-                            git log --pretty=format:"%h" | head -1
-                            """,
-                            returnStdout: true
-                        ).trim()
+                        commitID = sh(script: "git log --pretty=format:\"%h\" | head -1", returnStdout: true).trim()
 
-                        // Define variable for container build
                         config.b_config.imageTag = commitID
                         config.b_config.imageLatestTag = "latest"
-
                         config.commitID = commitID
 
-                        if ( config.b_config.containsKey("sequentialDeploymentMapping") ) {
+                        if (config.b_config.containsKey("sequentialDeploymentMapping")) {
                             config.sequential_deployment_mapping = config.b_config.sequentialDeploymentMapping[config.target_branch]
                         }
                     }
@@ -99,118 +78,86 @@ def call(Map config) {
 
             stage("Change Version Number") {
                 when {
-                    expression {
-                        return config.b_config.controllers.versionNumberController
-                    }
+                    expression { return config.b_config.controllers.versionNumberController }
                 }
                 steps {
                     script {
-                        lib_versionController(
-                            config
-                        )
+                        lib_versionController(config)
                     }
                 }
             }
 
             stage("Run Unit tests") {
                 when {
-                    expression {
-                        return config.b_config.controllers.unitTestController && 
-                            config.b_config.controllers.buildController
-                    }
+                    expression { return config.b_config.controllers.unitTestController && config.b_config.controllers.buildController }
                 }
                 steps {
                     script {
-                        lib_unitTestController(
-                            config
-                        )
+                        lib_unitTestController(config)
                     }
                 }
             }
 
             stage("Build Project") {
                 when {
-                    expression {
-                        return config.b_config.controllers.buildController
-                    }
+                    expression { return config.b_config.controllers.buildController }
                 }
                 steps {
                     script {
-                        lib_buildController(
-                            config
-                        )
+                        lib_buildController(config)
                     }
                 }
             }
 
             stage("Build and Publish as a Container") {
                 when {
-                    expression {
-                        return config.b_config.controllers.containerController
-                    }
+                    expression { return config.b_config.controllers.containerController }
                 }
                 steps {
                     script {
-                        lib_containerController(
-                            config
-                        )
+                        lib_containerController(config)
                     }
                 }
             }
 
             stage("Security Scan For Container") {
                 when {
-                    expression {
-                        return config.scan_container_image
-                    }
+                    expression { return config.scan_container_image }
                 }
                 steps {
-                    sh """
-                    echo ${config.containerImages.join("\n")} > anchore_images
-                    """
+                    sh "echo ${config.containerImages.join("\n")} > anchore_images"
                     anchore name: "anchore_images", bailOnFail: false
                 }
             }
-
         }
 
         post {
             always {
-                // Take necessary actions
                 script {
-                    // Cleanup
-                    lib_cleanupController(
-                        config
-                    )
-
-                    lib_postbuildController(
-                        config
-                    )
+                    lib_cleanupController(config)
+                    lib_postbuildController(config)
                 }
             }
             success {
                 script {
-                    def buildTime = getBuildTime()
-                    notify('Success', "The build has completed successfully in ${buildTime}.", 'teams-webhook-url')
+                    def buildTime = lib_teamsnotifications.getBuildTime()
+                    lib_teamsnotifications('Success', "The build has completed successfully in ${buildTime}.", 'teams-webhook-url')
                 }
                 script {
-                    def publisher = LastChanges.getLastChangesPublisher "PREVIOUS_REVISION", "SIDE", "LINE", true, true, "", "", "", "", ""
+                    def publisher = LastChanges.getLastChangesPublisher("PREVIOUS_REVISION", "SIDE", "LINE", true, true, "", "", "", "", "")
                     publisher.publishLastChanges()
                     def htmlDiff = publisher.getHtmlDiff()
                     writeFile file: 'build-diff.html', text: htmlDiff
 
-                    lib_helper.triggerJob(
-                        config
-                    )
+                    lib_helper.triggerJob(config)
                 }
             }
             failure {
                 script {
-                    def buildTime = getBuildTime()
-                    notify('Failure', "The build has failed after ${buildTime}. Please check the logs for details.", 'teams-webhook-url')
+                    def buildTime = lib_teamsnotifications.getBuildTime()
+                    lib_teamsnotifications('Failure', "The build has failed after ${buildTime}. Please check the logs for details.", 'teams-webhook-url')
                 }
             }
         }
-
     }
 }

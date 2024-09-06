2a25a095e4139173ae4cb30d6610e17e716fe815
Ferhat Kacar <ferhatkacar.partner@kafein.com.tr>
2024-09-06 23:51:10 +0300

    Notifications now add to deployments

--- vars/core_branchDeployer.groovy
+++ vars/core_branchDeployer.groovy
@@ -97,27 +97,35 @@ def call(Map config) {
                     }
                 }
             }
-
         }
 
         post {
             always {
-                // Take necessary actions
                 script {
-                    // Cleanup
-                    lib_cleanupController(
-                        config
-                    )
-
-                    lib_postbuildController(
-                        config
-                    )
+                    lib_cleanupController(config)
+                    lib_postbuildController(config)
                 }
             }
             success {
-                buildDescription("Container ID: ${env.CONTAINER_IMAGE_ID}")
+                script {
+                    def buildTime = lib_teamsnotifications.getBuildTime()
+                    lib_teamsnotifications('Success', "The deployment has completed successfully in ${buildTime}.", 'teams-webhook-url')
+                }
+                script {
+                    def publisher = LastChanges.getLastChangesPublisher("PREVIOUS_REVISION", "SIDE", "LINE", true, true, "", "", "", "", "")
+                    publisher.publishLastChanges()
+                    def htmlDiff = publisher.getHtmlDiff()
+                    writeFile file: 'build-diff.html', text: htmlDiff
+
+                    lib_helper.triggerJob(config)
+                }
+            }
+            failure {
+                script {
+                    def buildTime = lib_teamsNotifications.getBuildTime()
+                    lib_teamsNotifications.notify('Failure', "The build has failed after ${buildTime}. Please check the logs for details.", 'teams-webhook-url')
+                }
             }
         }
-
     }
 }

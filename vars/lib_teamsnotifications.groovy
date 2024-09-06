02bcfefa9d9ef7c1ff623032a821da400afdf593
Ferhat Kacar <ferhatkacar.partner@kafein.com.tr>
2024-09-06 19:59:56 +0300

    Updated lib

--- vars/lib_teamsnotifications.groovy
+++ vars/lib_teamsnotifications.groovy
@@ -1,89 +1,112 @@
-def notify(String status, String message, String credentialsId) {
-  def colors = [
-      'Success': 'Good', // green
-      'Failure': 'Attention' // red
-  ]
+// vars/myPipeline.groovy
+def call(String status, String message, String credentialsId) {
+    def colors = [
+        'Success': 'Good',
+        'Failure': 'Attention'
+    ]
 
-  if (!colors.containsKey(status)) {
-      error "Invalid status: $status"
-  }
+    def getBuildTime() {
+        def durationMillis = currentBuild.duration ?: 0
+        def durationSeconds = (durationMillis / 1000).toLong()
+        def minutes = (durationSeconds / 60).toLong()
+        def seconds = (durationSeconds % 60).toLong()
+        return "${minutes}m ${seconds}s"
+    }
 
-  def color = colors[status]
+    if (!colors.containsKey(status)) {
+        error "Invalid status: $status"
+    }
 
-  def title = "Jenkins Build Notification"
+    def color = colors[status]
+    def title = "Jenkins Build Notification"
+    def jobName = env.JOB_NAME
+    def jobLink = env.BUILD_URL
+    def buildNumber = env.BUILD_NUMBER
 
-  withCredentials([string(credentialsId: "teams-webhook-url", variable: 'webhookUrl')]) {
-      def payload = [
-        "type": "message",
-        "attachments": [
-            [
-              "contentType": "application/vnd.microsoft.card.adaptive",
-              "content": [
-                  "\$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
-                  "type": "AdaptiveCard",
-                  "version": "1.3",
-                  "body": [
-                    [
-                        "type": "TextBlock",
-                        "size": "Medium",
-                        "weight": "Bolder",
-                        "text": "${title}"
-                    ],
-                    [
-                        "type": "ColumnSet",
-                        "columns": [
-                          [
-                              "type": "Column",
-                              "items": [
-                                [
-                                    "type": "Image",
-                                    "style": "Person",
-                                    "url": "https://ftp-chi.osuosl.org/pub/jenkins/art/jenkins-logo/1024x1024/logo.png",
-                                    "altText": "Jenkins",
-                                    "size": "Medium"
+    withCredentials([string(credentialsId: "${credentialsId}", variable: 'webhookUrl')]) {
+        def payload = [
+            "type": "message",
+            "attachments": [
+                [
+                    "contentType": "application/vnd.microsoft.card.adaptive",
+                    "content": [
+                        "\$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+                        "type": "AdaptiveCard",
+                        "version": "1.3",
+                        "body": [
+                            [
+                                "type": "TextBlock",
+                                "size": "Medium",
+                                "weight": "Bolder",
+                                "text": "${title}"
+                            ],
+                            [
+                                "type": "ColumnSet",
+                                "columns": [
+                                    [
+                                        "type": "Column",
+                                        "items": [
+                                            [
+                                                "type": "Image",
+                                                "style": "Person",
+                                                "url": "https://ftp-chi.osuosl.org/pub/jenkins/art/jenkins-logo/1024x1024/logo.png",
+                                                "altText": "Jenkins",
+                                                "size": "Medium"
+                                            ]
+                                        ],
+                                        "width": "auto"
+                                    ],
+                                    [
+                                        "type": "Column",
+                                        "items": [
+                                            [
+                                                "type": "TextBlock",
+                                                "weight": "Bolder",
+                                                "text": "Jenkins Notification",
+                                                "wrap": true
+                                            ]
+                                        ],
+                                        "width": "auto"
+                                    ]
                                 ]
-                              ],
-                              "width": "auto"
-                          ],
-                          [
-                              "type": "Column",
-                              "items": [
-                                [
-                                    "type": "TextBlock",
-                                    "weight": "Bolder",
-                                    "text": "Jenkins Notification",
-                                    "wrap": true
-                                ]
-                              ],
-                              "width": "auto"
-                          ]
+                            ],
+                            [
+                                "type": "TextBlock",
+                                "weight": "Bolder",
+                                "color": "${color}",
+                                "text": "${status}",
+                                "wrap": true
+                            ],
+                            [
+                                "type": "TextBlock",
+                                "text": "${message}",
+                                "wrap": true
+                            ],
+                            [
+                                "type": "TextBlock",
+                                "weight": "Bolder",
+                                "text": "Job Name: ${jobName} (Build #${buildNumber})",
+                                "wrap": true
+                            ],
+                            [
+                                "type": "TextBlock",
+                                "weight": "Bolder",
+                                "text": "[Click here to view the job](${jobLink})",
+                                "wrap": true
+                            ]
                         ]
-                    ],
-                    [
-                        "type": "TextBlock",
-                        "weight": "Bolder",
-                        "color": "${color}",
-                        "text": "${status}",
-                        "wrap": true
-                    ],
-                    [
-                        "type": "TextBlock",
-                        "text": "${message}",
-                        "wrap": true
                     ]
-                  ]
-              ]
+                ]
             ]
         ]
-      ]
 
-      def response = httpRequest(
-        httpMode: 'POST',
-        contentType: 'APPLICATION_JSON',
-        requestBody: groovy.json.JsonOutput.toJson(payload),
-        url: webhookUrl
-      )
+        def response = httpRequest(
+            httpMode: 'POST',
+            contentType: 'APPLICATION_JSON',
+            requestBody: groovy.json.JsonOutput.toJson(payload),
+            url: webhookUrl
+        )
 
-      echo "Response: ${response}"
-  }
+        echo "Response: ${response}"
+    }
 }
